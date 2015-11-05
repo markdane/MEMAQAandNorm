ed1e70497f3fe20fe1075c77104266b5544376a0
Mark Dane <dane@ohsu.edu>
2015-11-05 13:49:05 -0800

    Remove unneeded files from repo

--- AnalysisWrapper.R
+++
@@ -1,7 +0,0 @@
-library("rmarkdown")
-
-for(cellLine in c("MCF7")){
-  for(ss in c("SS1")){
-    render("MEP-LINCS_Analysis.Rmd", output_file = paste0("Mep-LINCS_Analysis_",cellLine,"_",ss,".html"), output_format = "html_document") 
-  }
-}
