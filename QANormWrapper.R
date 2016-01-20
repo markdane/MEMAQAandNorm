2aeed5ccec6a24535efa0bdb4883dca162a12238
Mark Dane <dane@ohsu.edu>
2016-01-20 10:37:31 -0800

    Added non-working support for staining set normalization

--- QANormWrapper.R
+++ QANormWrapper.R
@@ -356,7 +356,9 @@ dataFiles <- data.frame(CellLine=rep(c("PC3", "YAPC","MCF7"),each=4),
                         StainingSet = rep(c("SS2","SS3","SS3","SS3","SS3"), each=12),
                         Signal=rep(c("EdU","LineageRatioLog2","DNA2N","SCC","Ecc"), each=12),
                         Method=c("RZS","NaiveRandRUV", "NaiveReplicateRUV","RUV3"),
-                        inputFileName=rep(c("../MEP-LINCS/PC3/SS2/AnnotatedData/PC3_SS2_Level1.txt", "../MEP-LINCS/YAPC/SS2/AnnotatedData/YAPC_SS2_Level1.txt", "../MEP-LINCS/MCF7/SS2/AnnotatedData/MCF7_SS2_Level1.txt","../MEP-LINCS/PC3/SS3/AnnotatedData/PC3_SS3_v1.1_Level1.txt", "../MEP-LINCS/YAPC/SS3/AnnotatedData/YAPC_SS3_Level1.txt", "../MEP-LINCS/MCF7/SS3/AnnotatedData/MCF7_SS3_Level1.txt","../MEP-LINCS/PC3/SS3/AnnotatedData/PC3_SS3_v1.1_Level1.txt", "../MEP-LINCS/YAPC/SS3/AnnotatedData/YAPC_SS3_Level1.txt", "../MEP-LINCS/MCF7/SS3/AnnotatedData/MCF7_SS3_Level1.txt","../MEP-LINCS/PC3/SS3/AnnotatedData/PC3_SS3_v1.1_Level1.txt", "../MEP-LINCS/YAPC/SS3/AnnotatedData/YAPC_SS3_Level1.txt", "../MEP-LINCS/MCF7/SS3/AnnotatedData/MCF7_SS3_Level1.txt","../MEP-LINCS/PC3/SS3/AnnotatedData/PC3_SS3_v1.1_Level1.txt", "../MEP-LINCS/YAPC/SS3/AnnotatedData/YAPC_SS3_Level1.txt", "../MEP-LINCS/MCF7/SS3/AnnotatedData/MCF7_SS3_Level1.txt"), each=4),stringsAsFactors = FALSE)[1,]
+                        Unit =c("Plate","Plate","Array","Plate"),
+                        inputFileName=rep(c("../MEP-LINCS/PC3/SS2/AnnotatedData/PC3_SS2_Level1.txt", "../MEP-LINCS/YAPC/SS2/AnnotatedData/YAPC_SS2_Level1.txt", "../MEP-LINCS/MCF7/SS2/AnnotatedData/MCF7_SS2_Level1.txt","../MEP-LINCS/PC3/SS3/AnnotatedData/PC3_SS3_v1.1_Level1.txt", "../MEP-LINCS/YAPC/SS3/AnnotatedData/YAPC_SS3_Level1.txt", "../MEP-LINCS/MCF7/SS3/AnnotatedData/MCF7_SS3_Level1.txt","../MEP-LINCS/PC3/SS3/AnnotatedData/PC3_SS3_v1.1_Level1.txt", "../MEP-LINCS/YAPC/SS3/AnnotatedData/YAPC_SS3_Level1.txt", "../MEP-LINCS/MCF7/SS3/AnnotatedData/MCF7_SS3_Level1.txt","../MEP-LINCS/PC3/SS3/AnnotatedData/PC3_SS3_v1.1_Level1.txt", "../MEP-LINCS/YAPC/SS3/AnnotatedData/YAPC_SS3_Level1.txt", "../MEP-LINCS/MCF7/SS3/AnnotatedData/MCF7_SS3_Level1.txt","../MEP-LINCS/PC3/SS3/AnnotatedData/PC3_SS3_v1.1_Level1.txt", "../MEP-LINCS/YAPC/SS3/AnnotatedData/YAPC_SS3_Level1.txt", "../MEP-LINCS/MCF7/SS3/AnnotatedData/MCF7_SS3_Level1.txt"), each=4),stringsAsFactors = FALSE)
+
 
 x <- dataFiles
 
@@ -366,4 +368,30 @@ callQANorm <- function(x){
          output_format = "html_document") 
 }
 
-apply(dataFiles, 1, callQANorm)
+#apply(dataFiles, 1, callQANorm)
+
+#Setup and run with unit as staining set
+ssDataFiles <- data.frame(CellLine=rep(c("PC3", "YAPC","MCF7"),each=3),
+                          StainingSet = c("SS1","SS2","SS3"),
+                          Signal=rep(c("SCC")),
+                          Method=c("NaiveReplicateRUVSS"),
+                          Unit ="StainingSet",
+                          inputFileName=c("../MEP-LINCS/PC3/SS1/AnnotatedData/PC3_SS1_Level1.txt",
+                                          "../MEP-LINCS/PC3/SS2/AnnotatedData/PC3_SS2_Level1.txt",
+                                          "../MEP-LINCS/PC3/SS3/AnnotatedData/PC3_SS3_v1.1_Level1.txt",
+                                          "../MEP-LINCS/YAPC/SS1/AnnotatedData/YAPC_SS1_Level1.txt",
+                                          "../MEP-LINCS/YAPC/SS2/AnnotatedData/YAPC_SS2_Level1.txt",
+                                          "../MEP-LINCS/YAPC/SS3/AnnotatedData/YAPC_SS3_Level1.txt",
+                                          "../MEP-LINCS/MCF7/SS1/AnnotatedData/MCF7_SS1_Level1.txt",
+                                          "../MEP-LINCS/MCF7/SS2/AnnotatedData/MCF7_SS2_Level1.txt",
+                                          "../MEP-LINCS/MCF7/SS3/AnnotatedData/MCF7_SS3_Level1.txt"), stringsAsFactors = FALSE)
+
+callSSQANorm <- function(x){
+  render(paste0("Mep-LINCS_QANorm.Rmd"),
+         output_file = paste0("Mep-LINCS_QANorm_",x[["CellLine"]],"_",x[["Signal"]],"_",x[["Method"]],".html"),
+         output_format = "html_document") 
+}
+
+for(cellLine in unique(ssDataFiles$CellLine)){
+  callSSQANorm(x=ssDataFiles[ssDataFiles$CellLine==cellLine,])
+}
